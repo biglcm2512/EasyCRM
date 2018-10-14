fdb99c8c77167d2f3e6061387747af485cdebdbb
biglcm2512 <37116056+biglcm2512@users.noreply.github.com>
2018-10-13 19:33:52 -0500

    Add files via upload

---
+++ Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyCrm.Models
+{
+   public class Usuario
+
+    {
+       // public int cedula { get; set; }
+        public string user{ get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string curso { get; set; }
+        public string fecha { get; set; }
+    }
+}
